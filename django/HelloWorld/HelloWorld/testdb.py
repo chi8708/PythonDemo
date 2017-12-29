ac9cad2d64459ba2f1e933a398585932f8b3b41d
chi8708 <983143462@qq.com>
2017-12-29 17:08:00 +0800

    使用pylint-django插件解决pylint的一些不必要的错误提示

--- django/HelloWorld/HelloWorld/testdb.py
+++ django/HelloWorld/HelloWorld/testdb.py
@@ -15,10 +15,10 @@ def getdata(request):
     # 通过objects这个模型管理器的all()获得所有数据行，相当于SQL中的SELECT * FROM
     list = Test.objects.all()
     # filter相当于SQL中的WHERE，可设置条件过滤结果
-    response2 = Test.objects.filter(id=1)
+    # response2 = Test.objects.filter(id=1)
 
     # 获取单个对象
-    response3 = Test.objects.get(id=1)
+    # response3 = Test.objects.get(id=1)
 
     # 限制返回的数据 相当于 SQL 中的 OFFSET 0 LIMIT 2;
     Test.objects.order_by('name')[0:2]
@@ -49,15 +49,16 @@ def edit(request):
 
     return HttpResponse("修改成功")
 
+
 def remove(request):
     # 删除id=1的数据
     test1 = Test.objects.get(id=1)
     test1.delete()
-    
+
     # 另外一种方式
     # Test.objects.filter(id=1).delete()
-    
+
     # 删除所有数据
     # Test.objects.all().delete()
-    
+
     return HttpResponse("<p>删除成功</p>")
